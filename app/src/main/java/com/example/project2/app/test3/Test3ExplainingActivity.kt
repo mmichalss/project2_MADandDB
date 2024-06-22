a1db78c06747cfc5e40349ed11d2b0cf7892099d
Justy <justynaonceagainforgothermail@gmail.com>
2024-06-22 22:50:54 +0200

    game 3 with a 5 by 5 grid and  final edits to game 2

---
+++ app/src/main/java/com/example/project2/app/test3/Test3ExplainingActivity.kt
@@ -0,0 +1,30 @@
+package com.example.project2.app.test3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.project2.R
+import com.example.project2.app.test2.Test2Activity
+
+class Test3ExplainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_test3_explaining)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var goToActivity = findViewById<Button>(R.id.button)
+        goToActivity.setOnClickListener {
+            val intent = Intent(this, Test3Activity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
